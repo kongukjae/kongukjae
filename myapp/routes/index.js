df7aa1f9af154fef139efce2a9a75a678d5220b6
kongukjae <kongukjae@gmail.com>
2021-03-16 15:59:57 +0900

    TEST pug engine

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'kongukjae' });
+  res.render('index', {
+    title: 'kongukjae',
+    myName: 'hello'
+  });
 });
 
 module.exports = router;
