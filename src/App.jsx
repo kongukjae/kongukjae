746689d0257669d244c1e413b9a8745667e2cb8c
kongukjae <51992661+kongukjae@users.noreply.github.com>
2024-10-08 03:11:21 +0900

    fix : 버그 해결 "

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <h1>공욱재</h1>
     </div>
   );
