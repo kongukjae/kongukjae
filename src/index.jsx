f559ab29a3f9b2e7203b602dc6f2cdf18706a9f5
kongukjae <51992661+kongukjae@users.noreply.github.com>
2024-10-08 02:54:48 +0900

    bugfix : 작업 수정

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
